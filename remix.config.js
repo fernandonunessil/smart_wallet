ac64517be5ff8f79d9685c1abe93cd6d018b202e
Fernando <fernandonunesjs@hotmail.com>
2024-02-06 15:59:10 -0300

    feat: flat routes

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,13 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
-};
+const { flatRoutes } = require('remix-flat-routes')
+
+/**
+ * @type {import("@remix-run/dev").AppConfig}
+ */
+module.exports = {
+  // ignore all files in routes folder to prevent
+  // default remix convention from picking up routes
+  ignoredRouteFiles: ['**/*'],
+  routes: async defineRoutes => {
+    return flatRoutes('routes', defineRoutes)
+  },
+}
