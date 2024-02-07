1cdd88f00030e9f0022f559b6fdf0f10b13182eb
Fernando <fernandonunesjs@hotmail.com>
2024-02-07 15:44:38 -0300

    style: add icon on Login Title

--- app/routes/_auth+/login.tsx
+++ app/routes/_auth+/login.tsx
@@ -5,7 +5,11 @@ export default function login() {
   return (
     <div className="w-full h-screen bg-zinc-800 flex justify-between items-center gap-3 px-6 text-white">
       <div className="w-1/2">
-        <h2 className="text-3xl p-2 font-semibold">Sua Carteira Pessoal</h2>
+        <div className="flex items-center pl-4">
+          {/* <i className="ri-wallet-3-line text-4xl"></i> */}
+          <i className="ri-money-dollar-box-fill text-4xl"></i>
+          <h2 className="text-3xl p-2 font-semibold">Sua Carteira Pessoal</h2>
+        </div>
         <p className="p-3 text-justify">
           {" "}
           Em um mundo cada vez mais conectado, a gestão financeira pessoal
