08f76648fd34d37c6f46cbecab8167ce2e1827a7
Fernando <fernandonunesjs@hotmail.com>
2024-02-08 12:37:50 -0300

    feat: card

---
+++ app/components/Card/Card.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { CardProps } from "types/Components/Card/type";
+
+export default function Card({ title, value, icon }: CardProps) {
+  return (
+    <div className="w-full border-zinc-300 border text-center shadow-2xl rounded-lg
+     p-4">
+      <div className="p-6">
+        <i className={`${icon} text-3xl`}></i>
+        <h2 className="text-xl">{title}</h2>
+        <p
+          className={`text-2xl ${
+            value < 0 ? "text-red-600" : "text-green-600"
+          }`}
+        >
+          {value}
+        </p>
+      </div>
+    </div>
+  );
+}
