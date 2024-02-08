ee5286ea61fcc4e6b7495a53fe01031d6541ee05
Fernando <fernandonunesjs@hotmail.com>
2024-02-08 17:02:09 -0300

    feat: dashboard cards

--- app/routes/_app+/dashboard+/_index.tsx
+++ app/routes/_app+/dashboard+/_index.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import Card from "~/components/Card/Card";
 
 export default function index() {
-  return <div>_index </div>;
+  return (
+    <div className="w-full">
+      <div className="p-4 flex items-center justify-between w-full gap-8 overflow-x-auto">
+        <Card title="Carteira" value={166.06} icon="ri-wallet-3-line" />
+        <Card title="Investimentos" value={692.06} icon="ri-wallet-3-line" />
+        <Card title="Entradas" value={1800} icon="ri-wallet-3-line" />
+        <Card title="Saidas" value={-732.32} icon="ri-wallet-3-line" />
+      </div>
+    </div>
+  );
 }
