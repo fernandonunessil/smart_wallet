58b04a8fb97ea9a60e27ba946f6e8f165b73387a
Fernando <fernandonunesjs@hotmail.com>
2024-02-19 17:00:22 -0300

    feat: receita atual

--- app/routes/_app+/revenue+/_index.tsx
+++ app/routes/_app+/revenue+/_index.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function index() {
   return (
-    <div>_index</div>
-  )
+    <div className="w-[86%]">
+      <div className="w-full text-2xl text-center p-4">
+        <h2>Receita Atual</h2>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
 }
