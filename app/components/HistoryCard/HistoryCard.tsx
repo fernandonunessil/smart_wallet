461e81af46e0b8ef6fd161fb0115b551ddb54a6c
Fernando Nunes <fernandonunesjs@hotmail.com>
2024-03-17 16:20:11 -0300

    feat: button adicionar saida/entrada

--- app/components/HistoryCard/HistoryCard.tsx
+++ app/components/HistoryCard/HistoryCard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HistoryCard() {
   return (
     <div className="w-full h-[400px] overflow-y-scroll shadow-xl border border-zinc-300 rounded">
-      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400  ">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 w-full sticky top-0">
           <tr>
             <th scope="col" className="px-6 py-3">
@@ -18,100 +18,100 @@ export default function HistoryCard() {
           </tr>
         </thead>
         <tbody className="h-[50px] overflow-y-scroll">
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+          <tr className="bg-white border-b y-700">
             <th
               scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
             >
               Apple MacBook Pro 17"
             </th>
             <td className="px-6 py-4">Silver</td>
             <td className="px-6 py-4">Laptop</td>
           </tr>
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+          <tr className="bg-white border-b 0 y-700">
             <th
               scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
             >
               Microsoft Surface Pro
             </th>
             <td className="px-6 py-4">White</td>
             <td className="px-6 py-4">Laptop PC</td>
           </tr>
-          <tr className="bg-white dark:bg-gray-800">
+          <tr className="bg-white ">
             <th
               scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
             >
               Magic Mouse 2
             </th>
             <td className="px-6 py-4">Black</td>
             <td className="px-6 py-4">Accessories</td>
           </tr>
-          <tr className="bg-white dark:bg-gray-800">
+          <tr className="bg-white">
             <th
               scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
             >
               Magic Mouse 2
             </th>
             <td className="px-6 py-4">Black</td>
             <td className="px-6 py-4">Accessories</td>
           </tr>
-          <tr className="bg-white dark:bg-gray-800">
+          <tr className="bg-white ">
             <th
               scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
             >
               Magic Mouse 2
             </th>
             <td className="px-6 py-4">Black</td>
             <td className="px-6 py-4">Accessories</td>
           </tr>
-          <tr className="bg-white dark:bg-gray-800">
+          <tr className="bg-white ">
             <th
               scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
             >
               Magic Mouse 2
             </th>
             <td className="px-6 py-4">Black</td>
             <td className="px-6 py-4">Accessories</td>
           </tr>
-          <tr className="bg-white dark:bg-gray-800">
+          <tr className="bg-white ">
             <th
               scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
             >
               Magic Mouse 2
             </th>
             <td className="px-6 py-4">Black</td>
             <td className="px-6 py-4">Accessories</td>
           </tr>
-          <tr className="bg-white dark:bg-gray-800">
+          <tr className="bg-white ">
             <th
               scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
             >
               Magic Mouse 2
             </th>
             <td className="px-6 py-4">Black</td>
             <td className="px-6 py-4">Accessories</td>
           </tr>
-          <tr className="bg-white dark:bg-gray-800">
+          <tr className="bg-white ">
             <th
               scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
             >
               Magic Mouse 2
             </th>
             <td className="px-6 py-4">Black</td>
             <td className="px-6 py-4">Accessories</td>
           </tr>
-          <tr className="bg-white dark:bg-gray-800">
+          <tr className="bg-white ">
             <th
               scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
             >
               Magic Mouse 2
             </th>
--- app/routes/_app+/wallet+/_index.tsx
+++ app/routes/_app+/wallet+/_index.tsx
@@ -6,15 +6,27 @@ export default function index() {
   return (
     <div className="w-[86%] p-4">
       <div className="p-4">
-        <Card title="Valor Atual" value={10000000} icon="" />
+        <Card title="Valor Atual na Carteira" value={10000000} icon="" />
       </div>
       <div className="p-4 flex items-center justify-between gap-8">
+        {/* Card de saida */}
         <div className="text-center w-full">
-          <h2 className="text-2xl p-2">Saidas</h2>
+          <div className="flex items-center justify-between p-2">
+            <h2 className="text-2xl p-2">Saidas</h2>
+            <button className="bg-zinc-700 text-white p-2 rounded">
+              Adicionar Saida
+            </button>
+          </div>
           <HistoryCard />
         </div>
+        {/* Card de Entrada */}
         <div className="text-center w-full">
-          <h2 className="text-2xl p-2">Entradas</h2>
+          <div className="flex items-center justify-between p-2">
+            <h2 className="text-2xl p-2">Entradas</h2>
+            <button className="bg-zinc-700 text-white p-2 rounded">
+              Adicionar Entrada
+            </button>
+          </div>
           <HistoryCard />
         </div>
       </div>
